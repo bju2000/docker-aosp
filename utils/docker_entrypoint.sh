7ebbdda50413448bf0e2e008e9dd3c7a81563869
bju2000 <bju2000@gmail.com>
2019-04-11 12:21:48 +0900

    Update docker_entrypoint.sh

--- utils/docker_entrypoint.sh
+++ utils/docker_entrypoint.sh
@@ -19,7 +19,7 @@ if [ -z ${GROUP_ID+x} ]; then GROUP_ID=1000; fi
 
 # ccache
 export CCACHE_DIR=/tmp/ccache
-export USE_CCACHE=1
+#export USE_CCACHE=1
 
 msg="docker_entrypoint: Creating user UID/GID [$USER_ID/$GROUP_ID]" && echo $msg
 groupadd -g $GROUP_ID -r aosp && \
